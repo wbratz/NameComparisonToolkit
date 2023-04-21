cb379103c547d1e2dbe713dd44cee0e3262d94cc
William Bratz <williambratz615@gmail.com>
2023-04-20 20:04:52 -0700

    Fixes issue where middle names are empty and intersect returns false

--- NameComparisonToolkit/Comparers/ExactMatch.cs
+++ NameComparisonToolkit/Comparers/ExactMatch.cs
@@ -53,13 +53,13 @@ public sealed class ExactMatch : ComparerBase
 
 	public override bool Contains(Name x, string y)
 		=> y.Contains(string.Join(" ", x.FirstName))
-		&& y.Contains(string.Join(" ", x.MiddleName))
-		&& y.Contains(string.Join(" ", x.LastName))
-		&& y.Contains(string.Join(" ", x.Suffix));
+			&& y.Contains(string.Join(" ", x.MiddleName))
+			&& y.Contains(string.Join(" ", x.LastName))
+			&& y.Contains(string.Join(" ", x.Suffix));
 
-	public override bool Intersects(Name x, Name y) 
+	public override bool Intersects(Name x, Name y)
 		=> y.FirstName.Intersect(x.FirstName).Any()
-			&& y.MiddleName.Intersect(x.MiddleName).Any()
+			&& ((!y.MiddleName.Any() && !x.MiddleName.Any()) || y.MiddleName.Intersect(x.MiddleName).Any())
 			&& y.LastName.Intersect(x.LastName).Any()
 			&& CompareRequiredString(x.Suffix, y.Suffix);
 }
