0646fb3b9aa301eab29c8e4b9b922911c1c2bfe8
William Bratz <williambratz615@gmail.com>
2023-04-21 15:54:45 -0700

    Updates Name Tests

--- NameComparisonToolkit.Tests/NameTests.cs
+++ NameComparisonToolkit.Tests/NameTests.cs
@@ -21,71 +21,63 @@ public class NameTests
 		var name1 = new Name("John", "Adam", "Smith", "Jr.");
 		var name2 = new Name("John", "Adam", "Smith", "Jr.");
 
-		var results = name1.Matches(name2).ToList();
-
-		foreach (var result in results)
-		{
-			//if (result.Method.Equals("ExactMatch"))
-			//{
-			//	result.IsMatch.Should().Be(true);
-			//}
-		}
-
-		//name1.Compare(name2).IsMatch.Should().BeTrue();
+		var results = name1.Matches(name2);
+		results.Where(x => x.ComparisonType.Equals(ComparisonType.ExactMatch)).First().IsMatch.Should().BeTrue();
 	}
 
-	// [Fact]
-	// public void Matches_ShouldReturnTrueForMixedSuffixPunctuation()
-	// {
-	// 	var name1 = new Name("John", "Adam", "Smith", "Jr");
-	// 	var name2 = new Name("John", "Adam", "Smith", "Jr.");
-	//
-	// 	name1.Compare(name2).IsMatch.Should().BeTrue();
-	// }
-	//
-	// [Fact]
-	// public void Matches_ShouldReturnFalseForNonMatchingNames()
-	// {
-	// 	var name1 = new Name("John", "Adam", "Smith", "Jr.");
-	// 	var name2 = new Name("John", "Adam", "Doe", "Jr.");
-	//
-	// 	name1.Compare(name2).IsMatch.Should().BeFalse();
-	// }
-	//
-	//
-	// [Fact]
-	// public void GetMatchResults_ShouldReturnResultCount_EqualToComparisonTypeCount()
-	// {
-	// 	var typeCount = Enum.GetNames(typeof(ComparisonType)).Length;
-	// 	var name1 = new Name("John", "Adam", "Smith", "Jr.");
-	// 	var name2 = new Name("John", "Adam", "Smith", "Jr.");
-	//
-	// 	var result = name1.Matches(name2);
-	//
-	// 	result.Count().Should().Be(typeCount);
-	// }
-	//
-	// [Fact]
-	// public void GetContainResults_ShouldReturnResultCount_EqualToComparisonTypeCount()
-	// {
-	// 	var typeCount = Enum.GetNames(typeof(ComparisonType)).Length;
-	// 	var name1 = new Name("John", "Adam", "Smith", "Jr.");
-	// 	var name2 = new Name("John", "Adam", "Smith", "Jr.");
-	//
-	// 	var result = name1.Contains(name2);
-	//
-	// 	result.Count().Should().Be(typeCount);
-	// }
-	//
-	// [Fact]
-	// public void GetIntersectResults_ShouldReturnResultCount_EqualToComparisonTypeCount()
-	// {
-	// 	var typeCount = Enum.GetNames(typeof(ComparisonType)).Length;
-	// 	var name1 = new Name("John", "Adam", "Smith", "Jr.");
-	// 	var name2 = new Name("John Adam", "Joel", "Smith", "Jr.");
-	//
-	// 	var result = name1.Intersects(name2);
-	//
-	// 	result.Count().Should().Be(typeCount);
-	// }
+	[Fact]
+	public void Matches_ShouldReturnTrueForMixedSuffixPunctuation()
+	{
+		var name1 = new Name("John", "Adam", "Smith", "Jr");
+		var name2 = new Name("John", "Adam", "Smith", "Jr.");
+
+		var results = name1.Matches(name2);
+		results.Where(x => x.ComparisonType.Equals(ComparisonType.ExactMatch)).First().IsMatch.Should().BeTrue();
+	}
+
+	[Fact]
+	public void Matches_ShouldReturnFalseForNonMatchingNames()
+	{
+		var name1 = new Name("John", "Adam", "Smith", "Jr.");
+		var name2 = new Name("John", "Adam", "Doe", "Jr.");
+
+		var results = name1.Matches(name2);
+		results.Where(x => x.ComparisonType.Equals(ComparisonType.ExactMatch)).First().IsMatch.Should().BeFalse();
+	}
+
+
+	[Fact]
+	public void GetMatchResults_ShouldReturnResultCount_EqualToComparisonTypeCount()
+	{
+		var typeCount = Enum.GetNames(typeof(ComparisonType)).Length;
+		var name1 = new Name("John", "Adam", "Smith", "Jr.");
+		var name2 = new Name("John", "Adam", "Smith", "Jr.");
+
+		var result = name1.Matches(name2);
+
+		result.Count().Should().Be(typeCount);
+	}
+
+	[Fact]
+	public void GetContainResults_ShouldReturnResultCount_EqualToComparisonTypeCount()
+	{
+		var typeCount = Enum.GetNames(typeof(ComparisonType)).Length;
+		var name1 = new Name("John", "Adam", "Smith", "Jr.");
+
+		var result = name1.Contains("name");
+
+		result.Count().Should().Be(typeCount);
+	}
+
+	[Fact]
+	public void GetIntersectResults_ShouldReturnResultCount_EqualToComparisonTypeCount()
+	{
+		var typeCount = Enum.GetNames(typeof(ComparisonType)).Length;
+		var name1 = new Name("John", "Adam", "Smith", "Jr.");
+		var name2 = new Name("John Adam", "Joel", "Smith", "Jr.");
+
+		var result = name1.Intersects(name2);
+
+		result.Count().Should().Be(typeCount);
+	}
 }
