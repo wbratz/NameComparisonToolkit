6788d995b3ea6512261a4b0603468c1046f814e2
Lillie Bradfield <lillie.bradfield@carvana.com>
2023-04-22 21:16:53 -0700

    test cases updated

--- NameComparisonToolkit.Tests/LastNameTests.cs
+++ NameComparisonToolkit.Tests/LastNameTests.cs
@@ -3,7 +3,7 @@ public class LastNameTests
 {
 	[Theory]
 	[InlineData("John", "Adam", "Smith Doe", "Jr.", true)]
-	[InlineData("John", "Adam", "Smith", "Jr.", false)]
+	[InlineData("john", "adam", "smith", "jr", false)]
 	[InlineData("John", "Adam", "Doe Smith", "Jr.", false)]
 	[InlineData("Joey", "Adam-levine", "smith Doe", "Junior", true)]
 	public void Matches_ShouldHandleMultipleLastNamesCorrectly(string firstName, string middleName, string lastName, string suffix, bool expectedResult)
@@ -18,7 +18,7 @@ public class LastNameTests
 
 	[Theory]
 	[InlineData("John", "Adam", "Smith Doe", "Jr.", "John", "Adam", "Doe Smith", "Jr.", true)]
-	[InlineData("John", "Adam", "Smith Doe", "Jr.", "John", "Adam", "Smith", "Jr.", false)]
+	[InlineData("John", "Adam", "Smith Doe", "Jr.", "john", "Adam", "Smith", "Jr.", false)]
 	[InlineData("John", "Adam", "Smith Doe", "Jr.", "John", "Adam", "Doe", "Jr.", false)]
 	public void EqualsIgnoreOrder_ShouldHandleMultipleLastNamesCorrectly(string firstName1, string middleName1, string lastName1, string suffix1, string firstName2, string middleName2, string lastName2, string suffix2, bool expectedResult)
 	{
@@ -42,6 +42,7 @@ public class LastNameTests
 	[InlineData("John", "Adam", "Smith James", "Jr.", "John", "Adam", "Smith", "Jr.", true)]
 	[InlineData("John", "Adam", "Smith", "Jr.", "John", "Adam", "Smith", "Jr", true)]
 	[InlineData("John", "Adam", "smith", "Jr.", "John", "Adam", "Smith von duff", "Jr", true)]
+	[InlineData("John", "Adam", "smith VON DUFF", "Jr.", "James", "Adam Levine", "Smith", "Jr", true)]
 	public void Intersects_ShouldCompareLastNamesCorrectly(string firstName1, string middleName1, string lastName1, string suffix1, string firstName2, string middleName2, string lastName2, string suffix2, bool expectedResult)
 	{
 		var name1 = new Name(firstName1, middleName1, lastName1, suffix1);
